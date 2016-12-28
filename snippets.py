b486c69808b5d5d5433f23f78bf84623c2d1f570
Beyonce Knowles <alina.verbenchuk@gmail.com>
2016-12-28 15:08:21 +0000

    Commit 2

--- snippets.py
+++ snippets.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
@@ -25,7 +26,25 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    arguments = parser.parse_args()
 
-if __name__ == "__main__":
-    main()
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subparser for the put command
+    logging.debug("Constructing put subparser")
+    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    put_parser.add_argument("name", help="Name of the snippet")
+    put_parser.add_argument("snippet", help="Snippet text")
+
+    arguments = parser.parse_args()
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
+
+  
