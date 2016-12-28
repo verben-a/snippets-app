8eeb7b595512f769b4e0f7b905263af322072602
Beyonce Knowles <alina.verbenchuk@gmail.com>
2016-12-28 14:43:24 +0000

    Initial commit

---
+++ snippets.py
@@ -0,0 +1,31 @@
+import logging
+
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with an associated name.
+
+    Returns the name and the snippet
+    """
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    return name, snippet
+    
+def get(name):
+    """Retrieve the snippet with a given name.
+
+    If there is no such snippet, return '404: Snippet Not Found'.
+
+    Returns the snippet.
+    """
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return ""
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
